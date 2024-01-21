defeda430b9104a7708dc1e3db1b207d25374c17
ericjyr <ericjyr@gmail.com>
2024-01-20 21:25:22 -0700

    Changes made to Project component. Added StockSavvy.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
@@ -53,7 +50,6 @@ export const NavBar = () => {
               <div className="social-icon">
               <a href="https://www.linkedin.com/in/e-reb/"><img src={navIcon1} alt="LinkedIn icon" /></a>
             <a href="https://github.com/ericjyr"><img src={navIcon2} alt="Github icon" /></a>
-
               </div>
             </span>
           </Navbar.Collapse>
